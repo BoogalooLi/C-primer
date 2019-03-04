2b9983c5a40cecfea2b03bbe190c9e44803c1688
LiYanchen <fz20111018@gmail.com>
2019-03-04 16:14:15 +0800

    Create 10.16.cpp

---
+++ Chapter 10/10.16.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void elimDups(vector<string>& words){
+    sort(words.begin(), words.end());
+    auto end_unique = unique(words.begin(), words.end());
+    words.erase(end_unique, words.end());
+}
+
+void biggies(vector<string> & words, vector<string>::size_type sz){
+    elimDups(words);
+    stable_sort(words.begin(), words.end(), [](const string& s1, const string& s2){return s1.size() < s2.size();});
+    auto wc = find_if(words.begin(), words.end(), [sz](const string& s){return s.size() >= sz; } );
+    for_each(wc, words.end(), [](const string& s){cout<<s<<" ";});
+    cout<<endl;
+}
+
+
+int main() {
+    string str;
+    vector<string> vec = {"the", "quick", "red", "fox", "jumps", "over", "the", "slow", "red", "turtle"};
+    biggies(vec, 5);
+
+}
