16153e182e66d3de6c1530bdd9f00e624b29009c
LiYanchen <fz20111018@gmail.com>
2019-03-04 16:23:07 +0800

    Create 10.17.cpp

---
+++ Chapter 10/10.17.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string> 
+#include"Sales_data"
+
+using namespace std;
+
+void print(const vector<string>& v){
+	for(const auto & c : v)
+		cout<<c<<" ";
+	cout<<endl;
+}
+
+void elimDup(vector<string>& v){
+	sort(v.begin(), v.end());
+	auto end_unique = unique(v.begin(), v.end());
+	v.erase(end_unique, v.end());
+}
+
+
+
+int main()
+{
+	vector<Sales_data> vec ;//no examples here, so no test for this scripts
+	
+	elimDup(vec);
+	stable_sort(vec.begin(), vec.end(), [](const Sales_data& s1, const Sales_data& s2){return s1.isbn() < s2.isbn(); });
+	print(vec);
+}
+
