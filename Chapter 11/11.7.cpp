a439e591c043e0af96fda099f0767ccef33a5c02
LiYanchen <563797067@qq.com>
2019-03-18 15:38:55 +0800

    Create 11.7.cpp

---
+++ Chapter 11/11.7.cpp
@@ -0,0 +1,15 @@
+#include<map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	map<string, std::vector<string> > family;
+	std::vector<string> name;
+	string family_name;
+	family.insert({family_name, name});
+
+	return 0;
+}
