00e6434466971cba9ed1f9509284fbc9b94b17dd
LiYanchen <563797067@qq.com>
2019-05-14 14:23:07 +0800

    Create 12.24.cpp

---
+++ Chapter12/12.24.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<memory>
+
+using namespace std;
+
+void ReadCin(size_t size){
+    string str;
+    unique_ptr<char[]> uptr(new char[size]);
+    while(cin>>str){
+        for(auto i = 0; i <= str.size(); ++i)
+            uptr[i] = str[i];
+    }
+    for(auto i = 0; i <= str.size(); ++i)
+        cout<<uptr[i]<<endl;
+}
+
+int main(){
+    ReadCin(10);
+}
