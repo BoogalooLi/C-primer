47264dcdbf477d696ce1dad5e86820f1aad97e94
LiYanchen <563797067@qq.com>
2019-07-06 15:23:02 +0800

    Create 16.49&50.cpp

---
+++ Chapter 16/16.49&50.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+template <typename T>
+string debug_rep(const T& t){
+	ostringstream ret;
+	ret<<t;
+	return ret.str();
+} 
+
+template <typename T> void f(T) { cout<<"f(T)"<<endl; }
+template <typename T> void g(T) { cout<<"g(T)"<<endl; }
+template <typename T> void f(const T*) { cout<<"f(const T*)"<<endl; }
+template <typename T> void g(T*) { cout<<"g(T*)"<<endl; }
+
+int main(){
+	int i = 42;
+	int* p1 = &i;
+	const int ci = 0;
+	const int* p2 = &ci;
+	
+	g(42);
+	f(42);
+	
+	g(p1);
+	f(p1);
+	
+	g(ci);
+	f(ci);
+	
+	g(p2);
+	f(p2);
+}
